00b2cbb49849c17207e5d5ef2dfdd87d018b51f7
anddroid82 <anddroid82@gmail.com>
2023-10-13 12:02:25 +0200

    v1

---
+++ src/main/java/hu/webuni/bookshop/dto/LoginDto.java
@@ -0,0 +1,15 @@
+package hu.webuni.bookshop.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class LoginDto {
+	
+	private String username;
+	private String password;
+
+}
--- src/main/java/hu/webuni/bookshop/security/JwtLoginController.java
+++ src/main/java/hu/webuni/bookshop/security/JwtLoginController.java
@@ -5,9 +5,11 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import hu.webuni.bookshop.dto.LoginDto;
 import lombok.AllArgsConstructor;
 
 @RestController
@@ -19,10 +21,9 @@ public class JwtLoginController {
 	private AuthenticationManager authenticationManager;
 	
 	@PostMapping
-	public String login(@RequestParam String username,@RequestParam String password) {
-		System.out.println("hello");
+	public String login(@RequestBody LoginDto login) {
 		Authentication authentication = authenticationManager.authenticate(
-				new UsernamePasswordAuthenticationToken(username, password)
+				new UsernamePasswordAuthenticationToken(login.getUsername(), login.getPassword())
 		);
 		return "\""+ jwtService.creatJwtToken((UserDetails)authentication.getPrincipal()) + "\"";
 	}
